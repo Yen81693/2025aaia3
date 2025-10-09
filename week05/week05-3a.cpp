86cb990fd4081b91ee8112ea9bb23d55a2f761b9
Yen81693 <114201603+Yen81693@users.noreply.github.com>
2025-10-09 17:07:03 +0800

    week05

---
+++ week05/week05-3a.cpp
@@ -0,0 +1,13 @@
+/// week05-3a.cpp Part1: Input Part2:Output
+/// CPE的第2題 UVA 483 倒過來
+#include <iostream>
+using namespace std;
+int main()
+{
+    string line; /// 一行字的字串 Part 1: Input
+    while ( getline(cin, line)){ ///讀進來
+
+        cout << line << endl; ///Part 2: Output
+    }
+
+}
